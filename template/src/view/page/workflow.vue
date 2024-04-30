d2e626934c0fb2aa0caa402ea5319ba78533412b
makeyangcom <open@wileho.com>
2024-04-30 22:12:34 +0800

    main

--- template/src/view/page/workflow.vue
+++ template/src/view/page/workflow.vue
@@ -1,10 +1,17 @@
 <template>
-    <main class="page-workflow" :class="page.current">workflow</main>
+    <main class="page-workflow" :class="page.current">
+        <ResizablePanelGroup direction="horizontal" class="workflow-main h-full items-stretch">
+            <ResizablePanel class="left">1</ResizablePanel>
+            <ResizableHandle with-handle />
+            <ResizablePanel class="right">2</ResizablePanel>
+        </ResizablePanelGroup>
+    </main>
 </template>
 
 <script setup lang="ts">
 import {onBeforeMount, onMounted, onBeforeUnmount, onUnmounted, nextTick} from "vue";
 import type {BaseStruct, PageStruct} from "@/package/struct";
+import {ResizableHandle, ResizablePanel, ResizablePanelGroup} from "@/package/ui/resizable";
 
 const props: any = defineProps<{
     base: BaseStruct,
@@ -31,5 +38,11 @@ onUnmounted(() => {});
 }
 .page-workflow.workflow{
     display: block;
+}
+.page-workflow .workflow-main .left{
+    flex: 0 0 280px !important;
+}
+.page-workflow .workflow-main .right{
+    flex: 0 0 calc(100% - 280px) !important;
 }
 </style>
